cad2b5663e1d1c73412659de4695c8a01661eb3f
devil-1964 <devgoe5115@gmail.com>
2024-05-12 01:11:24 +0530

    Added Details component

--- adoptMe/src/App.jsx
+++ adoptMe/src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css'
 import Pet from './Pet'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Logo from './Logo';
 import SearchParam from "./SearchParam";
-
+import Details from './Details';
 
 function App() {
 
   return (
-    <>
-      <Logo/>
-      <SearchParam />
-    </>
+    <BrowserRouter>
+
+      <Logo />
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParam />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
