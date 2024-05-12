ebd17590310cc6c850f7fd1798cca6c4dc024762
devil-1964 <devgoe5115@gmail.com>
2024-05-12 14:31:35 +0530

    Commited the changes to the local repository

--- adoptMe/src/App.jsx
+++ adoptMe/src/App.jsx
@@ -2,19 +2,34 @@ import './App.css'
 import Pet from './Pet'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Logo from './Logo';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import SearchParam from "./SearchParam";
 import Details from './Details';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+
+});
+
 
 function App() {
 
   return (
     <BrowserRouter>
-
-      <Logo />
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParam />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Logo />
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParam />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
